349ea75746eceedf91aaa283e403a1682592b489
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-01-13 22:17:51 +0800

    Version 1 January 13

---
+++ Week5/home.js
@@ -0,0 +1,6 @@
+function showDateTime() {
+    var currentDate = new Date();
+    var dateTimeString = 'Current Date and Time: ' + currentDate.toLocaleString(); document.getElementById('dateTime').innerHTML = dateTimeString;
+}
+
+window.alert("Welcome Aboard! Enjoy my not-so-well developed website!");
