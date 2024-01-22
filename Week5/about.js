368d0bd0235313c2e7a3e6a2e00d828bfd39fab9
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-01-22 21:50:25 +0800

    Javascript Lesson 17: Objects

--- Week5/about.js
+++ Week5/about.js
@@ -2,3 +2,11 @@ function showDateTime() {
     var currentDate = new Date();
     var dateTimeString = 'Current Date and Time: ' + currentDate.toLocaleString(); document.getElementById('dateTime').innerHTML = dateTimeString;
 }
+
+const person = {
+    name: "Angela 'SaiDa' Mae M. Tauyan",
+    age: 20
+};
+
+const personInfoElement = document.getElementById('personInfo');
+    personInfoElement.textContent = `I am ${person.name} and I'm ${person.age} years old.`;
