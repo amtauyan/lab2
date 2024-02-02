c3887273d31ef8dd1e4d578ed5dae1def67b21e4
Angela Mae Tauyan <amtauyan@student.apc.edu.ph>
2024-02-02 13:21:53 +0800

    added new fields to the table

---
+++ Week8/myguests.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 02, 2024 at 06:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `myguests`
+--
+
+CREATE TABLE `myguests` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `website` varchar(30) NOT NULL,
+  `comment` text NOT NULL,
+  `gender` enum('female','male','others') NOT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `myguests`
+--
+
+INSERT INTO `myguests` (`id`, `name`, `email`, `website`, `comment`, `gender`, `reg_date`) VALUES
+(1, 'John', 'john@example.com', 'Doe', '', 'female', '2024-01-30 04:50:35'),
+(2, 'Alyana', 'acreyes@example.com', 'Reyes', '', 'female', '2024-01-30 05:05:22'),
+(4, 'Pauleen Castro', 'pcastro@gmail.com', 'https://www.w3schools.com/', '', 'female', '2024-01-30 05:20:35'),
+(6, 'Chickie Maxino', 'ckmax@gmail.com', 'https://icloudph.com/cspc/sias', 'nice!', 'female', '2024-02-02 04:24:47'),
+(7, 'Shayne Lumapas', 'ramayyy@gmail.com', 'https://icloudph.com/cspc/sias', 'naks namern!', 'female', '2024-02-02 04:42:47');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `myguests`
+--
+ALTER TABLE `myguests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `myguests`
+--
+ALTER TABLE `myguests`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
